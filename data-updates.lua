51939cca9d8dba3e945e28df3edd05022d04300f
Em Karisch <em@khaos.solutions>
2025-09-17 23:55:38 +0200

    feat: remove circuit-network-2 technology and update unlocks to cybersyn train network

--- data-updates.lua
+++ data-updates.lua
@@ -1,2 +1,5 @@
 -- Better subgroup placement with SchallCircuitGroup
 require("data-updates.schall-circuit-group")
+
+-- Remove circuit-network-2 technology and move unlocks to cybersyn-train-network technology
+require("data-updates.remove-circuit-network-2-tech")
