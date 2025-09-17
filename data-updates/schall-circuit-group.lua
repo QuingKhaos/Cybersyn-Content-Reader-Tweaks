465e56695a2e0e7282e35fb8d4dfac4e28a46e9f
Em Karisch <em@khaos.solutions>
2025-09-17 23:45:13 +0200

    feat: add support for SchallCircuitGroup

---
+++ data-updates/schall-circuit-group.lua
@@ -0,0 +1,5 @@
+if mods["SchallCircuitGroup"] then
+  data.raw.item["cybersyn-provider-reader"].subgroup = "circuit-input"
+  data.raw.item["cybersyn-requester-reader"].subgroup = "circuit-input"
+  data.raw.item["cybersyn-delivery-reader"].subgroup = "circuit-input"
+end
